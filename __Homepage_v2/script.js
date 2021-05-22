f4549be83362209ca7978eed71fc66e242ec5beb
Jaybee18 <mister.jaybee18@gmail.com>
2021-05-23 00:16:29 +0200

    fixed and optimized content change animation

--- __Homepage_v2/script.js
+++ __Homepage_v2/script.js
@@ -1,46 +1,39 @@
 var index = 0;
 var elements = ["#temp", "#desciption", "#button"]
 var elements2 = ["#temp2", "#desciption2", "#button2"]
+var elementsslist = [elements, elements2]
 var contents = ["#firstcontent", "#secondcontent"]
 $(document).ready(function(){
     // hide all the contents that we don't want to display yet
     contents.slice(1).forEach(element => {
-        document.getElementById(element.substring(1)).style.display = "none";
+        //document.getElementById(element.substring(1)).style.display = "block";
         //document.getElementById(element.substring(1)).style.opacity = "0%";
     });
 });
 
-
-function right(){
-    index++;
-    index = index >= contents.length ? 0 : index;
-    if(index == 0){
-        document.getElementById(contents[index].substring(1)).style.display = "block";
-        document.getElementById(contents[index].substring(1)).style.opacity = "1";
-        document.getElementById(contents[index-1].substring(1)).style.display = "none";
-        document.getElementById(contents[index-1].substring(1)).style.opacity = "0";
-        hide_show_elements(elements, true);
-        hide_show_elements(elements2, false);
-    }else if(index == 1){
-        document.getElementById(contents[index].substring(1)).style.display = "block";
-        //document.getElementById(contents[index-1].substring(1)).style.display = "none";
-        hide_show_elements(elements, false, 0);
-        hide_show_elements(elements2, true, 1);
-    }
+function operationById(id){
+    var container = $(id);
+    container.removeClass("animationIN");
+    container.addClass("animationOUT");
+}
+function operation2ById(id){
+    var container = $(id);
+    container.removeClass("animationOUT");
+    container.addClass("animationIN");
 }
 
-function hide_show_elements(elemen, hide, additionaldelay){
-    if(!hide){
-        elemen.forEach(element => {
-            var container = $(element);
-            document.getElementById(element.substring(1)).style.animationDelay = (elements.indexOf(element)/10+additionaldelay).toString() + "s";
-            container.addClass("fadetoleft");
-        });
-    }else{
-        elemen.forEach(element => {
-            var container = $(element);
-            document.getElementById(element.substring(1)).style.animationDelay = (elements.indexOf(element)/10+additionaldelay).toString() + "s";
-            container.addClass("fadetoleftr");
-        });
-    }
+function left(){
+    operationById(contents[index]);
+    index++;
+    if(index >= elementsslist.length)
+        index = 0;
+    operation2ById(contents[index]);
+}
+
+function right(){
+    operationById(contents[index]);
+    index--;
+    if (index < 0)
+        index = elementsslist.length - 1;
+    operation2ById(contents[index]);
 }
